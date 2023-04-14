1d9bb475363dd4f401ced4bba8df779af361d047
Nicholi Shiell <nicholishiell@gmail.com>
2023-04-14 10:24:17 -0400

    create script to write sort data by symbol

--- Util/ExtractArticlesByStockSymbol.py
+++ Util/ExtractArticlesByStockSymbol.py
@@ -7,76 +7,79 @@ import Util
 ############################################################################################################
 # 
 ############################################################################################################
-def SearchForRowForSymbol(row, symbol):
-    searchResults = row['SEARCH_RESULTS']
+def AddSymbolsToDict(index, searchResults, symbolFoundDict):
     
-    if len(searchResults) > 0:
-        for item in searchResults:
-            itemSplit = item.split(':')
-            if itemSplit[0] == symbol:
-                return True
+    for item in searchResults:
+        itemSplit = item.split(':')
+        symbol = itemSplit[0]
 
-    return False
-
-############################################################################################################
-# 
-############################################################################################################
-def SearchDataFrameForSymbol(pdf, symbol):
-    
-    indexList = []
-            
-    for index, row in pdf.iterrows():       
-        if SearchForRowForSymbol(row,symbol):
-            indexList.append(index)
-    
-    return indexList
-
-############################################################################################################
-# 
-############################################################################################################
-def SaveResults(indexList, pdf, symbol):
-    
-    outputFilePath = './Output/'+symbol+'.csv' 
-    
-    outputFile = open(outputFilePath, 'a+')
-    
-    for index in indexList:
-        row = pdf.iloc[index]
-
-        cleanedArticle = Util.CleanWhiteSpace(row['Article'])
+        if symbol not in symbolFoundDict.keys():          
+            symbolFoundDict[symbol] = []
         
-        foundAt = cleanedArticle.find(symbol)
+        symbolFoundDict[symbol].append(index)
+
+############################################################################################################
+# 
+############################################################################################################
+def SaveResults(pdf, symbolFoundDict, outputDirPath, fileAccessType = 'a+'):
+    
+    # Loop over all the found symbols    
+    for symbol in symbolFoundDict:
+    
+        # open the file with the correct name
+        filename = symbol+'.csv'
+        outputFilePath = os.path.join(outputDirPath,filename)
+        outputFile = open(outputFilePath, fileAccessType)
+        
+        # get the list of rows which contain the symbol
+        listOfRows = symbolFoundDict[symbol]
+        
+        # loop over the rows and add them to the file
+        for index in listOfRows:
+        
+            row = pdf.iloc[index]
+
+            cleanedArticle = Util.CleanWhiteSpace(row['Article'])
+        
+            foundAt = cleanedArticle.find(symbol)
                 
-        outputString =  str(row['TimeStamp']) + ',' +  \
-                        str(row['URL']) + ',' + \
-                        str(row['Title']) + ',' + \
-                        cleanedArticle.strip() + ',' + \
-                        str(foundAt) + '\n'
-        
-        outputFile.write(outputString)
+            outputString =  str(row['TimeStamp']) + ',' +  \
+                            str(row['URL']) + ',' + \
+                            str(row['Title']) + ',' + \
+                            cleanedArticle.strip() + ',' + \
+                            str(foundAt) + '\n'
+            
+            outputFile.write(outputString)
+    
+        outputFile.close()
          
 ############################################################################################################
 # This searches a given pickle file for instances of the stock symbols and saves the output
 ############################################################################################################        
-def HandlePickleFile(pickleFilePath, symbolList):
+def HandlePickleFile(pickleFilePath, outputDirPath):
     print('Searching in: ', pickleFilePath)
     pdf = pd.read_pickle(pickleFilePath)
-    print(pdf.keys())
     
-    for symbol in symbolList:
-        print('Searching for:', symbol)
-        indexList = SearchDataFrameForSymbol(pdf, symbol)
-        SaveResults(indexList, pdf, symbol)      
+    # this dictionary keeps track of which stock symbols were found on which rows of the data frame
+    # key = symbol, value = list of rows indexs
+    symbolFoundDict = {}
+    
+    for index, row in pdf.iterrows():    
+        searchResults = row['SEARCH_RESULTS']
+        if len(searchResults) > 0:
+            AddSymbolsToDict(index, searchResults, symbolFoundDict)
+        
+    SaveResults(pdf, symbolFoundDict, outputDirPath)        
               
 ############################################################################################################
 # This searches a folder full of pickle files
 ############################################################################################################  
-def HandleFolderOfPickleFiles(folderPath, symbolList):
+def HandleFolderOfPickleFiles(folderPath, outputDirPath = './Output/'):
     listOfFiles = Util.listdir_nohidden(folderPath)
 
     for filename in listOfFiles:
         pickleFilePath = os.path.join(folderPath, filename)
-        HandlePickleFile(pickleFilePath, symbolList)
+        HandlePickleFile(pickleFilePath, outputDirPath)
 
 ############################################################################################################
 # Test Function:
@@ -84,14 +87,14 @@ def HandleFolderOfPickleFiles(folderPath, symbolList):
 def TestFunction():
     monthList = ['August', 'December', 'November', 'October', 'September']
     pickleFileDir = '/home/nickshiell/storage/BaChu/PickleJar/2016/'
-    tickerDictFilePath = '../TickerData/tickersCompanyNames.csv'
    
-    symbolList = Util.ReadTickerFile(tickerDictFilePath)
-
     for month in monthList:
         print('Month: ', month)
         searchLocation = os.path.join(pickleFileDir, month)
-        HandleFolderOfPickleFiles(searchLocation, symbolList)
+        HandleFolderOfPickleFiles(searchLocation)
         
+############################################################################################################
 
+# Uncomment the line of code below to run the TestFunction which demonstrates how the functions work.
+#TestFunction()
 
